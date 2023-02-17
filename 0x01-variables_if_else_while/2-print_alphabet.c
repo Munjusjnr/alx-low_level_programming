88949c9b06f5e9ef6afa44f7e537592e0cbd0c98
Munjusjnr <munjusjnr54@outlook.com>
2023-02-17 13:31:31 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-	 int i;
-	
-	 for (i = 97; i <= 122; i++)
-	 {
-	 putchar(i);
-	 }
-	 putchar('\n');
-	 return (0);
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+	putchar (i);
+	}
+	putchar ('\n');
+	return (0);
 }

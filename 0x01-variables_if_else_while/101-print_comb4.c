4afd39eb4e749f60841c1233957cabcd74bd718c
Munjusjnr <munjusjnr54@outlook.com>
2023-02-17 18:24:41 -0800

    three single combinations1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,23 +9,26 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 10; i++)
 	{
-	for (j = i + 1; j < 9; j++)
+	for (j = i + 1; j < 10; j++)
 	{
 	for (k = j + 1; k < 10; k++)
-	{
-	if (i != j && i != k && j != k)
 	{
 	putchar ('0' + i);
 	putchar ('0' + j);
 	putchar ('0' + k);
+	if (i == 7 && j == 8 && k == 9)
+	{
+	putchar ('\n');
+	}
+	else
+	{
 	putchar (',');
 	putchar (' ');
 	}
 	}
 	}
 	}
-	putchar ('\n');
 	return (0);
 }

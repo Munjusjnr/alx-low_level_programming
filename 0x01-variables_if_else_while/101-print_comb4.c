7e7ade0dc5ba551b0a95a2a677e8867ed2380ef4
Munjusjnr <munjusjnr54@outlook.com>
2023-02-17 17:41:42 -0800

    three single combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - This snippet outputs three single digit combinations
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+	for (j = i + 1; j < 9; j++)
+	{
+	for (k = j + 1; k < 10; k++)
+	{
+	if (i != j && i != k && j != k)
+	{
+	putchar ('0' + i);
+	putchar ('0' + j);
+	putchar ('0' + k);
+	putchar (',');
+	putchar (' ');
+	}
+	}
+	}
+	}
+	putchar ('\n');
+	return (0);
+}

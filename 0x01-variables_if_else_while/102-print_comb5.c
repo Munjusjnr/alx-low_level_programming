cb88a6f769ce85c905cc8abd71aecb1e59bba218
Munjusjnr <munjusjnr54@outlook.com>
2023-02-19 13:41:39 -0800

    two two-digit combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,21 +9,21 @@ int main(void)
 {
 	int i, j, k, l;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j <= 9; j++)
+	for (j = 0; j < 10; j++)
 	{
-	for (k = i; k <= 9; k++)
+	for (k = i; k < 10; k++)
 	{
 	l = (k == i) ? j + 1 : 0;
-	for (; 1 <= 9; l++)
+	for (; 1 < 9; l++)
 	{
 	putchar (i + '0');
 	putchar (j + '0');
 	putchar (' ');
 	putchar (k + '0');
 	putchar (l + '0');
-	if (i != 9 || j != 8 || k != 9 || l != 9)
+	if (i == 9 && j == 9 && k == 9 && l == 9)
 	{
 	putchar ('\n');
 	}

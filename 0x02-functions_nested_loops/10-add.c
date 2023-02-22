1b785933e058e89cd6a008803ff55670cb69d009
Munjusjnr <munjusjnr54@outlook.com>
2023-02-22 04:19:11 -0800

    summation of int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - prints the sum of integers
+ * *@b: first operand
+ * *@c: second operand
+ * Return: Sum of integers
+ */
+
+int add(int b, int c)
+{
+	int sum;
+
+	sum = b + c;
+
+	return (sum);
+}

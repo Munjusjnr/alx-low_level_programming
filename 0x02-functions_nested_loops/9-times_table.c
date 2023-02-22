f1375ce0d99430568a878c9aebc69fe4eed5bc4e
Munjusjnr <munjusjnr54@outlook.com>
2023-02-22 04:08:46 -0800

    nine times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - print times table for 9
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+	prod = i * j;
+	if (j == 0)
+	{
+	_putchar ('0');
+	}
+	else if (prod < 10)
+	{
+	_putchar (',');
+	-putchar (' ');
+	-putchar (' ');
+	_putchar (prod + '0');
+	}
+	else
+	{
+	_putchar (',');
+	_putchar (' ');
+	_putchar (prod / 10 + '0');
+	_putchar (prod % 10 + '0');
+	}
+	}
+	_putchar ('\n');
+	}
+}

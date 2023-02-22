ec99c6065c047da9bbc1e784d8a3a0cf58956ac2
Munjusjnr <munjusjnr54@outlook.com>
2023-02-22 05:23:29 -0800

    natural numbers4

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,25 +10,31 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	if (n < 0)
 	{
-	for (i = n; i < 98; i++)
+	for (i = n; i <= 98; i++)
 	{
-	_putchar (i);
+	_putchar (i / 10 + '0');
+	_putchar (i % 10 + '0');
+	if (i != 98)
+	{
 	_putchar (',');
 	_putchar (' ');
+	}
 	}
 	}
 	else
 	{
-	for (i = n; i > 98; i--)
+	for (i = n; i >= 98; i--)
 	{
-	_putchar (i);
+	_putchar (i / 10 + '0');
+	_putchar (i % 10 + '0');
+	if (i != 98)
+	{
 	_putchar (',');
 	_putchar (' ');
 	}
 	}
-
-	_putchar (98);
+	}
 	_putchar ('\n');
 }

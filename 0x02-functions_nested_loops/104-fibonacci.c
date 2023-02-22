1c10e9479465cbdeeb247808342c21ad9111e512
Munjusjnr <munjusjnr54@outlook.com>
2023-02-22 09:14:04 -0800

    first 98 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to code
+ * Description: This function prints out the first
+ * 98 fibonacci numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	double a, b, temp;
+	int i;
+
+	a = 1;
+	b = 2;
+
+	printf("%.0f, %.0f, ", a, b);
+	for (i = 3; i <= 98; i++)
+	{
+	temp = b;
+	b = a + b;
+	a = temp;
+	printf("%.0f", b)
+	if (i < 98)
+	{
+	printf(", ");
+	}
+	}
+	printf("\n");
+	return (0);
+}

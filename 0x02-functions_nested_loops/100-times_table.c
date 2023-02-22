b1f0a9689c2a08e41a42ac4759cab40f24d5e18c
Munjusjnr <munjusjnr54@outlook.com>
2023-02-22 06:37:54 -0800

    100 times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints time between 0 and 15
+ * * @n: operand
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int i, j, res;
+
+	if (n < 0 || n > 15)
+		return;
+	for (i = 0; i <= n; i++)
+	{
+	for (j = 0; j <= n; j++)
+	{
+	res = i * j;
+	if (j == 0)
+	{
+	_putchar ('0');
+	}
+	else
+	{
+	_putchar (',');
+	_putchar (' ');
+	if (res < 10)
+	{
+	_putchar (' ');
+	_putchar (' ');
+	_putchar (res + '0');
+	}
+	else if (res < 100)
+	{
+	_putchar (' ');
+	_putchar (res / 10 + '0');
+	_putchar (res % 10 + '0');
+	}
+	else
+	{
+	_putchar (res / 100 + '0');
+	_putchar (res % 100 / 10 + '0');
+	_putchar (res % 10 + '0');
+	}
+	}
+	}
+	_putchar ('\n');
+	}
+}

8bdfc277b863b313cca2c40de0895384029e472e
Munjusjnr <munjusjnr54@outlook.com>
2023-02-23 07:27:31 -0800

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers and exclude some
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	if (i == '2' || i == '4')
+	{
+	continue;
+	}
+	_putchar (i);
+	}
+	_putchar ('\n');
+}

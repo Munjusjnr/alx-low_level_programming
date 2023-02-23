701c7b4e370827b169d9ac7ce2036ecc74ad3ccd
Munjusjnr <munjusjnr54@outlook.com>
2023-02-23 09:07:34 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ * *@n: operand for length of line
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+	return;
+	}
+	for (i = 0; i < n; i++)
+	{
+	_putchar ('_');
+	}
+	_putchar ('\n');
+}

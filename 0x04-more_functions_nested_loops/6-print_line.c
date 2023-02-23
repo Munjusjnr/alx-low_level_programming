9b6fbece37fe5627f4c9380897fae73ec25a268c
Munjusjnr <munjusjnr54@outlook.com>
2023-02-23 10:25:56 -0800

    printing a line4

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,16 +10,12 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-	{
-	_putchar ('\n');
-	return;
-	}
-	else
-	{
 	for (i = 0; i < n; i++)
 	{
 	_putchar ('_');
+	if (n <= 0)
+	{
+	_putchar ('\n');
 	}
 	}
 }

1d1ba8c4b62620818b43462036cfa4ff99851344
Munjusjnr <munjusjnr54@outlook.com>
2023-02-23 12:08:18 -0800

    diagonal printing

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a given input diagonally
+ * *@n: operand for number of times
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+	for (j = 0; j < i; j++)
+	{
+	if (n <= 0)
+	{
+	_putchar ('\n');
+	}
+	_putchar (' ');
+	}
+	_putchar ('\\');
+	}
+	_putchar ('\n');
+}

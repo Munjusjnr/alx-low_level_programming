b182ec3df1735cb97facf4b92e8506e8021d8a27
Munjusjnr <munjusjnr54@outlook.com>
2023-02-23 12:21:37 -0800

    diagonal printing3

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,12 +10,12 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	for (i = 0; i < n; i++)
+	{
 	if (n <= 0)
 	{
 	_putchar ('\n');
 	}
-	for (i = 0; i < n; i++)
-	{
 	for (j = 0; j < i; j++)
 	{
 	_putchar (' ');

116d032aea31c08c3b0ad896c995f52c93233e77
Munjusjnr <munjusjnr54@outlook.com>
2023-02-23 18:39:21 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,25 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
+	if (x != 100)
+	{
+		printf(" ");
+	}
 	if (x % 3 == 0 && x % 5 == 0)
 	{
-		printf("FizzBuzz ");
+		printf("FizzBuzz");
 	}
 	else if (x % 3 == 0)
 	{
-		printf("Fizz ");
+		printf("Fizz");
 	}
 	else if (x % 5 == 0)
 	{
-		printf("Buzz ");
+		printf("Buzz");
 	}
 	else
 	{
-		printf("%d ", x);
+		printf("%d", x);
 	}
 	}
 	printf("\n");

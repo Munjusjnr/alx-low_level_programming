3acc58872fd8740b3630780a8094aaf30fa46a1e
Munjusjnr <munjusjnr54@outlook.com>
2023-02-27 09:08:38 -0800

    printing out  a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function prints out a string
+ * *@str: parameter for string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}

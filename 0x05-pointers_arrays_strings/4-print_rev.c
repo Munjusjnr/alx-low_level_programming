848a148edb9122e15b9e2890f3079cc2405bc183
Munjusjnr <munjusjnr54@outlook.com>
2023-02-27 09:48:50 -0800

    printing out in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * print_rev - print out a string in reverse
+ * *@s: operand to hold string
+ * Return: void
+ */
+
 void print_rev(char *s)
 {
 	int len, g;

99aaf7a509e960865c220cb73c434717f0729cc5
Munjusjnr <munjusjnr54@outlook.com>
2023-03-01 09:26:00 -0800

    outputting in num value compa. of strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - comparison of two strings
+ * *@s1: a pointer to first string
+ * *@s2: a pointer to second string
+ * Return: the results in numerical value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+
+}

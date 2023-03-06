ebb7c587079644c57a5386a2b6dd7c35a7309ae6
Munjusjnr <munjusjnr54@outlook.com>
2023-03-06 14:05:32 -0800

    This was sloppy

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * set_string - set value of a pointer to char
+ * @s: A pointer to a pointer char s
+ * @to: the character pointer to be copied
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s =  malloc(strlen(to) + 1);
+
+	strcpy(*s, to);
+}

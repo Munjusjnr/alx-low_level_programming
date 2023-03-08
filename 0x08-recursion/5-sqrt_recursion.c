dfef0d2e0df09867fa183032aff6fc2dd58ce28f
Munjusjnr <munjusjnr54@outlook.com>
2023-03-08 13:34:22 -0800

    square root function of a given int

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/* function declaration for _sqrt_helper function */
+int _sqrt_helper(int n, int start, int end);
+/**
+ * _sqrt_recursion - output the square root of a given integer
+ * *@n: operand for integer
+ * Return: The square_helper function computing the root of given int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (_sqrt_helper(n, 0, n));
+}
+/**
+ * _sqrt_helper - The function takes the given int and uses binary search by
+ * bisecting it
+ * *@n: operand for integer
+ * *@start: this indicates where the binary starts
+ * *@end: indicates the binary ends
+ * Return: The computed square root
+ */
+int _sqrt_helper(int n, int start, int end)
+{
+	int mid = (start + end) / 2;
+
+	int square = mid * mid;
+
+	if (start > end)
+	{
+		return (-1);
+	}
+	if (square == n)
+	{
+		return (mid);
+	}
+	else if (square < n)
+	{
+		return (_sqrt_helper(n, mid + 1, end));
+	}
+	else
+	{
+		return (_sqrt_helper(n, start, mid - 1));
+	}
+}

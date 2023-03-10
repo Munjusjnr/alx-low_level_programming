552aaf58da94e4e1aa0731b4d32ca9fc94619773
Munjusjnr <munjusjnr54@outlook.com>
2023-03-10 12:20:46 -0800

    addition of integer arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point to program with two arguments
+ * *@argc: Argument count
+ * *@argv: argument vector
+ * Return: 0 when success 1 if otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, x, pro;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		i = atoi(argv[1]);
+		x = atoi(argv[2]);
+		pro = i * x;
+		printf("%d\n", pro)
+	}
+	return (0);
+}

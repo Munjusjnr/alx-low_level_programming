a760a975001009ee4cb27727718dda026c26d0a7
Munjusjnr <munjusjnr54@outlook.com>
2023-03-10 07:56:53 -0800

    Arguments to main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Takes two arguments
+ * *@argc: An argument that returns the numerical value of argv
+ * *@argv: A pointer to character string
+ * Return: 0 when success
+ */
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

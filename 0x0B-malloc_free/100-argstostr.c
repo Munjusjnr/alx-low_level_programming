10851f98561e5a2c7c428ee1d9ee38e2e4998be3
Munjusjnr <munjusjnr54@outlook.com>
2023-03-15 08:04:17 -0700

    concatenating arguments and allocating memory

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - function that concatenate all arguments
+ * *@ac: Operand for argument count
+ * *@av: A pointer to a pointer character
+ * Return: Null if failure, arguments if success
+ */
+
+char *argstostr(int ac, char **av)
+{
+	size_t length;
+	int i, index;
+	char *result;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	length = 0;
+	for (i = 0; i < ac; i++)
+	{
+		length += strlen(av[i]) + 1;
+	}
+	result = malloc(sizeof(char) * length);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	index = 0;
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(result + index, av[i]);
+		index += strlen(av[i]);
+		result[index++] = '\n';
+	}
+	return (result);
+}

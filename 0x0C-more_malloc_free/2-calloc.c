db735b03bee967671b436c8bdc46b2c8abec7ab0
Munjusjnr <munjusjnr54@outlook.com>
2023-03-16 10:08:29 -0700

    creating array set at zero using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - function using malloc to set memory at zero for array
+ * *@nmemb: array of int
+ * *@size: size of array of int
+ * Return: A pointer to arr memory allocated or NUll
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(sizeof(nmemb) * size);
+	memset(arr, 0, sizeof(nmemb) * size);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	return (arr);
+}

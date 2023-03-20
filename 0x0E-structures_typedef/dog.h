2cc0d343e6cf6d8c0faac4b62d7fa289c8e03390
Munjusjnr <munjusjnr54@outlook.com>
2023-03-20 13:32:50 -0700

    struct given a new name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,8 @@ struct dog
 	float age;
 	char *owner;
 };
+/* New name for struct */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 

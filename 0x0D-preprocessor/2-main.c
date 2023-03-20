06439a44b44d598a2498fc14de850c39ed04d3d3
Munjusjnr <munjusjnr54@outlook.com>
2023-03-20 05:38:59 -0700

    name of compiled file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point to the program
+ * Description: outputs the name of the file it was compiled on
+ * Return: 0 when success
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}

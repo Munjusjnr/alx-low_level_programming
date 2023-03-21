f0cee86abac8ebe6847e29dce38bc5b921f654ce
Munjusjnr <munjusjnr54@outlook.com>
2023-03-20 18:35:51 -0700

    preventing memory leaks

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Free the allocated memory to prevent leaks
+ * *@d: A pointer to struct
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	d = NULL;
+}

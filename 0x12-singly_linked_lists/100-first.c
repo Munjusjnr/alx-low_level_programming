9b5b587ec11e8fee48a0034061260cdce4fec941
Munjusjnr <munjusjnr54@outlook.com>
2023-03-30 09:12:12 -0700

    a function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * before_main_function - a function that executes before main
+ * Return: void
+ */
+
+void before_main_function(void)
+{
+	printf("You're beat! and yet, you must");
+	printf(" allow,\nI bore my house upon my back!\n");
+}

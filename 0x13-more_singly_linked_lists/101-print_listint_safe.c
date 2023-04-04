ff3a5c3f93092e98314a0605b6686ff61662b3cf
Munjusjnr <munjusjnr54@outlook.com>
2023-04-04 16:55:30 -0700

    can't handle this task yet 1

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,31 +11,24 @@ size_t print_listint_safe(const listint_t *head)
 	size_t count = 0;
 	const listint_t *slow = head;
 	const listint_t *fast = head;
-	
+
 	if (head == NULL)
 		return (0);
 	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
 		printf("[%p] %d\n", (void *)slow, slow->n);
 		slow = slow->next;
-		fast = fast->next->next;
+		fast = fast->next;
 		if (slow == fast)
 		{
 		printf("-> [%p] %d\n", (void *)slow, slow->n);
 		count++;
 		slow = head;
-		while (slow != NULL && fast != NULL && slow != fast)
+		while (slow != fast)
 		{
 			printf("[%p] %d\n", (void *)slow, slow->n);
 			slow = slow->next;
-			if (fast != NULL && fast->next != NULL)
-			{
-				fast = fast->next->next;
-			}
-			else
-			{
-				fast = NULL;
-			}
+			fast = fast->next;
 		}
 		}
 		count++;

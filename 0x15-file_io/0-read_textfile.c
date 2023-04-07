a16d992efbf3a2e67222ead9db58474054af9716
Munjusjnr <munjusjnr54@outlook.com>
2023-04-07 07:13:56 -0700

    1 added to denote null character at the end of string

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	buffer = malloc(letters);
+	buffer = malloc(letters + 1);
 	if (buffer == NULL)
 	{
 		close(ptr);

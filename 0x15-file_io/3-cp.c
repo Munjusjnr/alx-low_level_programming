4324bb8c778c164f18e4e4e7d27d6f935903c77f
Munjusjnr <munjusjnr54@outlook.com>
2023-04-08 09:57:14 -0700

    changes mades to program

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,17 @@
 #include "main.h"
 #define BUFF_SIZE 1024
+#define MODE 0664
+/**
+ * outer - function that prints error messages
+ * @msg: A pointer to the character message
+ * @code: code to print out
+ * Return: void
+ */
+void outer(char *msg, int code)
+{
+	dprintf(STDERR_FILENO, "%s\n", msg);
+	exit(code);
+}
 /**
  * main - Entry point to code
  * Description: A program that copies from one file to another
@@ -15,41 +27,38 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
+		outer("Usage: cp file_from file_to", 97);
 	}
 	src = open(argv[1], O_RDONLY);
 	if (src < 0)
 	{
-		dprintf(2, "Error: Can't read from file argv[1]\n");
-		exit(98);
+		outer("Error: Can't read from file %s", 98);
 	}
-	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, MODE);
 	if (dest < 0)
 	{
-		dprintf(2, "Error: Can't write to argv[2]\n");
-		exit(99);
+		outer("Error: Can't write to %s", 99);
 	}
 	while (1)
 	{
 		toread = read(src, buffer, BUFF_SIZE);
 		if (toread <= 0)
+		{
 			break;
+		}
 		towrite = write(dest, buffer, toread);
 		if (towrite <= 0)
+		{
 			break;
+		}
 	}
-	close(src);
-	close(dest);
 	if ((close(src)) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", src);
-		exit(100);
+		outer("Error: Can't close fd %d", 100);
 	}
 	if ((close(dest)) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", dest);
-		exit(100);
+		outer("Error: Can't close fd %d", 100);
 	}
 	return (0);
 }

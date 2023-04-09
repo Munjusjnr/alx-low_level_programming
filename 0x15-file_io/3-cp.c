67ba6a11d63e8c030d8231e5782353072d36c4be
Munjusjnr <munjusjnr54@outlook.com>
2023-04-09 13:54:01 -0700

    changes made to code base

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,17 +1,5 @@
 #include "main.h"
 #define BUFF_SIZE 1024
-#define MODE 0664
-/**
- * outer - function that prints error messages
- * @msg: A pointer to the character message
- * @code: code to print out
- * Return: void
- */
-void outer(char *msg, int code)
-{
-	dprintf(STDERR_FILENO, msg, errno, strerror(errno));
-	exit(code);
-}
 /**
  * main - Entry point to code
  * Description: A program that copies from one file to another
@@ -27,37 +15,45 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		outer("Usage: cp file_from file_to\n", 97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 	src = open(argv[1], O_RDONLY);
 	if (src < 0)
 	{
-		outer("Error: Can't read from file %s\n", 98);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
 	}
-	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, MODE);
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (dest < 0)
 	{
-		outer("Error: Can't write to %s\n", 99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 	while ((toread = read(src, buffer, BUFF_SIZE)) > 0)
 	{
 		towrite = write(dest, buffer, toread);
 		if (towrite != toread)
 		{
-			outer("Error: Can't write to %s\n", 99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 		}
 	}
 	if (toread < 0)
 	{
-		outer("Error: Can't read from file %s\n", 98);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
 	}
 	if ((close(src)) < 0)
 	{
-		outer("Error: Can't close fd %d\n", 100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
+		exit(100);
 	}
 	if ((close(dest)) < 0)
 	{
-		outer("Error: Can't close fd %d\n", 100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest);
+		exit(100);
 	}
 	return (0);
 }

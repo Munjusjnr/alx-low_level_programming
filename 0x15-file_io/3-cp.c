545c904e1e88e8288f16e79f0c0c90d5d5e9779a
Munjusjnr <munjusjnr54@outlook.com>
2023-04-09 16:26:50 -0700

    function created to handle error messages

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,33 @@
 #include "main.h"
 #define BUFF_SIZE 1024
 /**
+ * handler - handles errors
+ * @errorcode: holds error code
+ * @file: A pointer to file
+ * @filed: file descriptor
+ * Return: void
+ */
+void handler(int errorcode, char *file, int filed)
+{
+	switch (errorcode)
+	{
+	case 97:
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		break;
+	case 98:
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		break;
+	case 99:
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		break;
+	case 100:
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filed);
+		break;
+	}
+	exit(errorcode);
+}
+
+/**
  * main - Entry point to code
  * Description: A program that copies from one file to another
  * @argc: The number of arguments to be supplied
@@ -15,45 +42,38 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		handler(97, NULL, 0);
 	}
 	src = open(argv[1], O_RDONLY);
 	if (src < 0)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
+		handler(98, argv[1], 0);
 	}
 	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (dest < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		handler(99, argv[2], 0);
 	}
 	while ((toread = read(src, buffer, BUFF_SIZE)) > 0)
 	{
 		towrite = write(dest, buffer, toread);
 		if (towrite != toread)
 		{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+			handler(99, argv[2], 0);
 		}
 	}
 	if (toread < 0)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
+		handler(98, argv[1], 0);
 	}
 	if ((close(src)) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
-		exit(100);
+		handler(100, NULL, src);
 	}
 	if ((close(dest)) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest);
-		exit(100);
+		handler(100, NULL, dest);
 	}
 	return (0);
 }

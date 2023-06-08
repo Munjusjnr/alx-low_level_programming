4f506a4737f9954ed15a1807a1c40c7a368aff5e
Munjusjnr <munjusjnr54@outlook.com>
2023-06-08 08:59:03 -0700

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_hash;
+	hash_table_t *new_hash = NULL;
 	unsigned long int i;
 
 	new_hash = malloc(sizeof(hash_table_t));

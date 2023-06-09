cba24f1af840597d291875e6173ca627f7fbec69
Munjusjnr <munjusjnr54@outlook.com>
2023-06-09 03:09:46 -0700

    function that prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - This function prints the hash table out
+ * @ht: table to be printed out
+ * Return: Null if failure hash table if success
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	int first_hnode = 1;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			if (!first_hnode)
+			{
+				printf(", ");
+			}
+			printf("%s: %s", current->key, current->value);
+			first_hnode = 0;
+			current = current->next;
+		}
+	}
+	printf("}\n");
+}

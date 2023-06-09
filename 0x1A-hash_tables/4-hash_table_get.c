a2822c71f53c79931ba02a3474eb4ca313b00498
Munjusjnr <munjusjnr54@outlook.com>
2023-06-09 02:28:04 -0700

    traversing through the list

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,17 +13,18 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *current;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
-	if (current != NULL)
+	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
 			return (current->value);
 		}
+		current = current->next;
 	}
 	return (NULL);
 }

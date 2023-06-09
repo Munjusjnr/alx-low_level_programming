cf477160e7885b8c520f0e899f6ce5052bb0c7a5
Munjusjnr <munjusjnr54@outlook.com>
2023-06-09 03:12:18 -0700

    apostrophe added to output string

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -24,7 +24,7 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				printf(", ");
 			}
-			printf("%s: %s", current->key, current->value);
+			printf("'%s': '%s'", current->key, current->value);
 			first_hnode = 0;
 			current = current->next;
 		}
